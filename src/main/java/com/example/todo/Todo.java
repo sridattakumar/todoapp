5bef15a0fd51abf0b6401c3638767e5e625d783b
sridattakumar <dattakumar1999@gmail.com>
2023-09-05 17:16:43 +0000

    solution

--- src/main/java/com/example/todo/Todo.java
+++ src/main/java/com/example/todo/Todo.java
@@ -4,13 +4,13 @@ package com.example.todo;
 // import java.util.*;
 
 public class Todo{
-    private int todoId;
+    private int id;
     private String todo;
     private String priority;
     private String status; 
 
     public Todo(int id, String todo, String priority, String status){
-        this.todoId = id;
+        this.id = id;
         this.todo = todo;
         this.priority = priority;
         this.status = status;
